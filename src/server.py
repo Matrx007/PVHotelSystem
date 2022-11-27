2953470381232b8c1702ecffdb8059bc35cbc270
Rainis Randmaa <raintz1000@gmail.com>
2022-11-27 17:04:06 +0200

    Create random/dumb API endpoints

--- src/server.py
+++ src/server.py
@@ -1,19 +1,53 @@
 from flask import Flask, render_template, request
 from flask_cors import CORS
+import os
+import random
+from test_data import ROOMS
 
+FLDR = os.path.abspath('./frontend/build')
 
 app = Flask(
     __name__,
-    template_folder='templates',
-    static_folder='static'
+    template_folder='frontend/build',
+    static_folder=FLDR,
+    static_url_path=""
 )
 
 CORS(app)
 
-@app.route("/")
-def root():
-    return 'At /'
+@app.route('/', defaults={'path': ''})
+@app.route("/<string:path>")
+@app.route('/<path:path>')
+def catch_all(path):
+    print("sending index.html");
+    print(FLDR);
+    return app.send_static_file("index.html")
 
+
+@app.route('/api/rooms')
+def get_rooms():
+
+    SHUFFLED_ROOMS = ROOMS.copy()
+    random.shuffle(SHUFFLED_ROOMS)
+    return SHUFFLED_ROOMS
+
+@app.route('/api/rooms-sorted')
+def get_rooms_sorted():
+    print("column:", request.args.get('column') or 'price')
+    print("isAscending:", request.args.get('isAscending') == 'true' or False)
+
+    SHUFFLED_ROOMS = ROOMS.copy()
+    random.shuffle(SHUFFLED_ROOMS)
+    return SHUFFLED_ROOMS
+
+@app.route('/api/rooms-available')
+def get_rooms_sorted():
+    print("column:", request.args.get('column') or 'price')
+    print("isAscending:", request.args.get('isAscending') == 'true' or False)
+
+    SHUFFLED_ROOMS = ROOMS.copy()
+    random.shuffle(SHUFFLED_ROOMS)
+    return SHUFFLED_ROOMS
 
 if __name__ == "__main__":
     app.run(
---
+++ src/test_data.py
@@ -0,0 +1,53 @@
+ROOMS = [
+        {
+            "name": "Ühekohaline",
+            "available": 4,
+            "price": 40,
+            "pictures": [
+                "https://images.trvl-media.com/lodging/75000000/74250000/74242600/74242531/9b2b2d66.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/5a476c74.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/9c4ce050.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium"
+            ]
+        },
+        {
+            "name": "Kahekohaline",
+            "available": 5,
+            "price": 75,
+            "pictures": [
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/1436d39c.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/bf3c0cbc.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/79035584.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium"
+            ]
+        },
+        {
+            "name": "Kahe voodiga",
+            "available": 3,
+            "price": 80,
+            "pictures": [
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/83c80c39.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/917201ab.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/c43c9335.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium"
+            ]
+        },
+        {
+            "name": "Kolmekohaline",
+            "available": 3,
+            "price": 100,
+            "pictures": [
+                "https://images.trvl-media.com/lodging/75000000/74250000/74242600/74242531/1331f409.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/42361b97.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/11995beb.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium"
+            ]
+        },
+        {
+            "name": "Sviit",
+            "available": 1,
+            "price": 150,
+            "pictures": [
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/9f0abbf8.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/bcb18a26.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/5dfe5424.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium",
+                "https://images.trvl-media.com/hotels/75000000/74250000/74242600/74242531/f5bad581.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium"
+            ]
+        }
+    ];
