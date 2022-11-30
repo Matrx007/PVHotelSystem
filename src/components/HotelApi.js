9cf87c34cd16c60773190d2a6354b17c347010b4
Andrus Vaher <andrusvaher2004@gmail.com>
2022-11-30 21:10:15 +0200

    updated SearchRooms.js

--- src/components/HotelApi.js
+++ src/components/HotelApi.js
@@ -191,4 +191,4 @@ async function getRoomsByAvailability(startDate, endDate) {
   ];
 }
 
-export { getRooms };
+export { getRooms, getRoomsSortedBy };
--- src/components/SearchRooms.js
+++ src/components/SearchRooms.js
@@ -5,15 +5,70 @@ import RoomCard from "./RoomCard";
 
 function SearchRooms() {
   const [rooms, setRooms] = useState([]);
+  const [sortType_1, setSortType_1] = useState("");
+  const [sortType_2, setSortType_2] = useState("");
 
   useEffect(() => {
     getRooms().then((data) => setRooms(data));
   }, []);
 
+  useEffect(() => {
+    const sortArray = (type) => {
+      const types = {
+        toad: "name",
+        saadavus: "available",
+        hind: "price",
+      };
+      const sortProperty = types[type];
+      const ascending = [...rooms].sort(
+        (a, b) => b[sortProperty] - a[sortProperty]
+      );
+      setRooms(ascending);
+    };
+    sortArray(sortType_1);
+  }, [sortType_1]);
+
+  useEffect(() => {
+    const sortArray = (type) => {
+      const types = {
+        toad: "name",
+        saadavus: "available",
+        hind: "price",
+      };
+      const sortProperty = types[type];
+      const descending = [...rooms].sort(
+        (a, b) => a[sortProperty] - b[sortProperty]
+      );
+      setRooms(descending);
+    };
+    sortArray(sortType_2);
+  }, [sortType_2]);
+
   return (
     <div>
       <OtherPagesNavbar />
-      <h1>Sirvi tube</h1>
+      <div className="search_elements">
+        <h1>Sirvi tube</h1>
+        <div>
+          <select id="#" onChange={(e) => setSortType_1(e.target.value)}>
+            <option value="sorteerimine" disabled selected>
+              Sorteerimine
+            </option>
+            <option value="toad">Toa tüüp</option>
+            <option value="saadavus">Saadavus</option>
+            <option value="hind">Hind</option>
+          </select>
+          <select id="space" onChange={(e) => setSortType_2(e.target.value)}>
+            <option value="sorteerimine" disabled selected>
+              Sorteerimine
+            </option>
+            <option value="toad">Toa tüüp</option>
+            <option value="saadavus">Saadavus</option>
+            <option value="hind">Hind</option>
+          </select>
+          <button className="search">Otsi saadavuse järgi</button>
+        </div>
+      </div>
       {rooms.map((room) => {
         return (
           <RoomCard
